1048d65ccc8a3437d48d06bba76d4c8087ca35d8
G-BlackStar <100958186+G-BlackStar@users.noreply.github.com>
2024-05-19 11:38:10 +0900

    Modifications ergonomiques
    Suppression des boites de dialogue type alert. Remplacement de ces dernières par du texte qui s'affiche après un intervalle de 2 secondes (Merci le cours sur le Javascript en question). Changement de quelques paramètres de hauteur sur style.css

--- script.js
+++ script.js
@@ -21,16 +21,28 @@ function getExtraHoursinMinutes(event) {
   const differenceInMinutes = actualEndTimeInMinutes - endTimeInMinutes;
   result.textContent = `T'as fait ${differenceInMinutes} minutes d'heures supplémentaires. / 残業 ${differenceInMinutes} 分`;
   if (differenceInMinutes < 0) {
-    comment.textContent = `Tu te fous de moi ?! / ふざけんな！`;
+    setTimeout(() => {
+      comment.textContent = `Tu te fous de moi ?! / ふざけんな！`;
+    }, 2000);
   } else if (differenceInMinutes <= 30) {
-    comment.textContent = `T'as eu de la chance ! / ラッキーじゃないか！`;
+    setTimeout(() => {
+      comment.textContent = `T'as eu de la chance ! / ラッキーじゃないか！`;
+    }, 2000);
   } else if (differenceInMinutes <= 60) {
-    comment.textContent = `Comme d'habitude hein? / 相変わらずだね`;
+    setTimeout(() => {
+      comment.textContent = `Comme d'habitude hein? / 相変わらずだね`;
+    }, 2000);
   } else if (differenceInMinutes <= 90) {
-    comment.textContent = `Faudrait p'tet lever le pied ! / やりすぎじゃない？`;
+    setTimeout(() => {
+      comment.textContent = `Faudrait p'tet lever le pied ! / やりすぎじゃない？`;
+    }, 2000);
   } else if (differenceInMinutes <= 120) {
-    comment.textContent = `Bon ça suffit maintenant ! / いい加減にしろ！`;
+    setTimeout(() => {
+      comment.textContent = `Bon ça suffit maintenant ! / いい加減にしろ！`;
+    }, 2000);
   } else {
-    comment.textContent = `Mais merde! Va dormir!! / はやく寝ろ！クソバカヤロウ！`;
+    setTimeout(() => {
+      comment.textContent = `Mais merde! Va dormir!! / はやく寝ろ！クソバカヤロウ！`;
+    }, 2000);
   }
 }
